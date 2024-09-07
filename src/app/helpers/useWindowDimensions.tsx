71d53e6034abbb0e461ff09a5cb7795df120578a
pereladov.ad <aleksandr.pereladov@simbirsoft.com>
2024-09-07 17:01:59 +0500

    fix spectrum 2

--- src/app/helpers/useWindowDimensions.tsx
+++ src/app/helpers/useWindowDimensions.tsx
@@ -1,39 +1,33 @@
 'use client'
 import { useState, useEffect } from 'react';
 
-// Интерфейс для размеров окна
-interface WindowDimensions {
-    width: number;
-    height: number;
-}
-
-export default function useWindowDimensions(): WindowDimensions {
-    // Функция для получения размеров окна
-    function getWindowDimensions(windowObj: (Window & typeof globalThis) | undefined) {
-        const width =  windowObj?.innerWidth || 0;
-        const height = windowObj?.innerHeight || 0;
-        return {
-            width,
-            height,
-        };
-    }
-
-    const [windowDimensions, setWindowDimensions] = useState<WindowDimensions>();
-
+export default function useWindowDimensions(): { width: undefined; height: undefined } {
+    const [windowSize, setWindowSize] = useState({
+        width: undefined,
+        height: undefined,
+    });
 
     useEffect(() => {
-        // Обновляем размеры окна при монтировании
-        setWindowDimensions(getWindowDimensions(window));
-
+        // only execute all the code below in client side
+        // Handler to call on window resize
         function handleResize() {
-            setWindowDimensions(getWindowDimensions(window));
+            // Set window width/height to state
+            setWindowSize({
+                // @ts-ignore
+                width: window.innerWidth,
+                // @ts-ignore
+                height: window.innerHeight,
+            });
         }
 
-        window.addEventListener('resize', handleResize);
+        // Add event listener
+        window.addEventListener("resize", handleResize);
 
-        // Очищаем обработчик при размонтировании
-        return () => window.removeEventListener('resize', handleResize);
-    }, []);
+        // Call handler right away so state gets updated with initial window size
+        handleResize();
 
-    return windowDimensions as WindowDimensions;
+        // Remove event listener on cleanup
+        return () => window.removeEventListener("resize", handleResize);
+    }, []); // Empty array ensures that effect is only run on mount
+    return windowSize;
 }
