d724c01d3266236e26ff3b05357d094f4dde7405
ALOHAHAWA11 <mastermarga1@gmail.com>
2024-09-07 17:44:06 +0400

    remove unused code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
 'use client'
-import React, {useState} from 'react';
 import Game from './components/game';
 
 function App() {
-  const [audioSrc, setAudioSrc] = useState<string | undefined>(undefined);
-  console.log(audioSrc)
   return (
       <div className="App">
-           <Game setAudioSrc={setAudioSrc}/>
+           <Game/>
       </div>
   );
 }
